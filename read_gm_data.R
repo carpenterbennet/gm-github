fd53e91003f79d829a3736013846f4ec64314d2e
Carpenter Bennet <bennet@umich.edu>
2016-05-02 22:12:40 -0400

    Reading 1957 data

--- read_gm_data.R
+++ read_gm_data.R
@@ -1,5 +1,7 @@
 # Read data from .csv files
 gdp <- read.csv("data/data-1952.csv")
+new <- read.csv("data/data-1957.csv")
+gdp <- rbind(gdp, new)
 
 # Calculate GDP
 gdp['gdp'] <- gdp$pop * gdp$gdpPercap
